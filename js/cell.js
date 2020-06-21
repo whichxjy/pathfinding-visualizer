669be12c930587d7ae64dc054425629ba72a70c0
whichxjy <whichxjy@gmail.com>
2020-06-21 11:51:38 +0800

    Init cell and map

---
+++ js/cell.js
@@ -0,0 +1,9 @@
+function Cell(row, col, x, y, width, height, isWall) {
+  this.row = row;
+  this.col = col;
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.isWall = isWall;
+}
---
+++ js/map.js
@@ -0,0 +1,29 @@
+function SearchMap(rowNum, colNum, x, y, width, height) {
+  this.rowNum = rowNum;
+  this.colNum = colNum;
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+
+  this.cellWidth = this.width / colNum;
+  this.cellHeight = this.height / rowNum;
+
+  // 2D maze
+  this.maze = new Array(rowNum);
+
+  for (let i = 0; i < rowNum; i++) {
+    this.maze[i] = new Array(colNum);
+  }
+
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const wallRatio = 0.4;
+      const isWall = random(1.0) < wallRatio;
+      this.maze[row][col] = new Cell(
+        row, col, this.x + row * this.cellWidth,
+        this.y + col * this.cellHeight, isWall
+      );
+    }
+  }
+}
